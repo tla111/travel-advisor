4787fed09e54fc5d24ef410437269b0f22cacd17
Tim La <tlamarketing111@gmail.com>
2022-03-08 11:18:42 -0500

    added styles for map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+
     return (
         <h1>Map</h1>
     )
