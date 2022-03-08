78d1545176cc40a730364e4570d66d4a8c0d1299
Tim La <tlamarketing111@gmail.com>
2022-03-08 11:45:18 -0500

    able display type & rating

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,6 +6,8 @@ import useStyles from './styles';
 const List = () => {
     const classes = useStyles();
     const [type, setType] = useState("restaurants");
+    const [rating, setRating] = useState("restaurants");
+
 
     return (
         <div className={classes.container}>
@@ -16,6 +18,15 @@ const List = () => {
                     <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>
                     <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
                 </Select>
             </FormControl>
         </div>
