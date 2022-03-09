b839fd694ae97bff4e609993e10754b4ae7ebcfb
Tim La <tlamarketing111@gmail.com>
2022-03-09 14:19:48 -0500

    able to show address with location icon

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
-import LocationOnOutlined from "@material-ui/icons/LocationOn";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 import PhoneIcon from "@material-ui/icons/Phone";
 import Rating from '@material-ui/lab/Rating';
 
@@ -36,6 +36,11 @@ const PlaceDetails = ({ place }) => {
                 {place?.cuisine?.map(({ name }) => (
                     <Chip key={name} size="small" label={name} className={classes.chip} />
                 ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon /> {place.address}
+                    </Typography>
+                )}
             </CardContent>
 
         </Card>
