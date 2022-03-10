48b4301c689d12ee56d63f958e27f442e724f118
Tim La <tlamarketing111@gmail.com>
2022-03-10 14:02:02 -0500

    error

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -7,8 +7,11 @@ import Rating from '@material-ui/lab/Rating';
 import useStyles from './styles';
 
 
-const PlaceDetails = ({ place }) => {
+const PlaceDetails = ({ place, selected, refProp }) => {
     const classes = useStyles();
+
+    if (selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+
 
     return (
         <Card elevation={6}>
@@ -19,6 +22,10 @@ const PlaceDetails = ({ place }) => {
             />
             <CardContent>
                 <Typography gutterBottom variant="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Rating value={Number(place.rating)} readOnly />
+                    <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+                </Box>
                 <Box display="flex" justifyContent="space-between">
                     <Typography variant="subtitle1">Price</Typography>
                     <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
